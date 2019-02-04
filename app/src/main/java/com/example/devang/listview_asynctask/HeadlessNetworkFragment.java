d800181ddfcd7357aa5105a0e31aee5b67bb6700
dv-android <devang.droid@gmail.com>
2019-02-04 22:40:52 +0530

    During orientation change , restoring UI state of list view by using sending 'serializable ArrayList object' through Bundle & use it in onRestoreInstance method done. Also restoring the current listview row position  by sending 'parcelable listview onSaveInstanceState object' through Bundle implemented

--- app/src/main/java/com/example/devang/listview_asynctask/HeadlessNetworkFragment.java
+++ app/src/main/java/com/example/devang/listview_asynctask/HeadlessNetworkFragment.java
@@ -22,7 +22,8 @@ public class HeadlessNetworkFragment extends Fragment implements LoadFeedData.Ta
     public void onCreate(@Nullable Bundle savedInstanceState){
        super.onCreate(savedInstanceState);
        setRetainInstance(true);
-       Log.d("HeadlessNetworkFragment","mDownloading = "+mDownloading);
+       Log.d("D :- Fragment","-onCreate");
+       //Log.d("HeadlessNetworkFragment","mDownloading = "+mDownloading);
 
        LoadFeedData loadFeedData = new LoadFeedData();
        loadFeedData.mTaskCallback = this;
@@ -31,25 +32,56 @@ public class HeadlessNetworkFragment extends Fragment implements LoadFeedData.Ta
     }
 
     @Override
-    public void taskFinish(ArrayList<Object> list){
-      mAsyncResponse.processFinish(list);
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        Log.d("D :- Fragment","-onActivityCreated");
     }
+
     @Override
     public void onAttach(Activity activity){
         super.onAttach(activity);
+        Log.d("D :- Fragment","-onAttach "+activity);
         mAsyncResponse = (AsyncResponse) activity;
 
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d("D :- Fragment","-onStart");
+    }
 
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d("D :- Fragment","-onPause");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d("D :- Fragment","-onResume");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d("D :- Fragment","-onStop");
+    }
 
     @Override
     public void onDetach(){
         super.onDetach();
+        Log.d("D :- Fragment","-onDetach");
     }
 
     @Override
     public void onDestroy(){
         super.onDestroy();
+        Log.d("D :- Fragment","-onDestroy");
+    }
+
+    @Override
+    public void taskFinish(ArrayList<Object> list){
+        mAsyncResponse.processFinish(list);
     }
 }
--- app/src/main/java/com/example/devang/listview_asynctask/MainActivity.java
+++ app/src/main/java/com/example/devang/listview_asynctask/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,18 +16,32 @@ import android.widget.CheckedTextView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 
 public class MainActivity extends ListActivity implements AsyncResponse {
 
     private HeadlessNetworkFragment mHeadlessNetworkFragment;
+
+    private ListAdapter adapter;
+
+    ArrayList<Object> arrayList;
+
+    Parcelable state;
+
+    String listState;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
 
-
+        if(savedInstanceState!=null){
+            state = savedInstanceState.getParcelable("listViewState");
+            Log.d("D :- Main Activity","-onRestoreInstanceState onCreate "+state);
+            getListView().onRestoreInstanceState(state);
+        }
+        Log.d("D :- MainActivity", "onCreate");
         mHeadlessNetworkFragment = (HeadlessNetworkFragment)getFragmentManager()
                 .findFragmentByTag("counter_fragment");
         if(mHeadlessNetworkFragment == null) {
@@ -34,18 +49,69 @@ public class MainActivity extends ListActivity implements AsyncResponse {
             getFragmentManager().beginTransaction().add(mHeadlessNetworkFragment, "counter_fragment").commit();
         }
         //mHeadlessNetworkFragment.mAsyncResponse = this;
-        Log.d("MainActivity", "onCreate called");
-        Log.d("MainActivity","Build SDK version is "+ Build.VERSION.SDK_INT);
+        //Log.d("MainActivity","Build SDK version is "+ Build.VERSION.SDK_INT);
 
 
 
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d("D :- Main Activity","-onStart");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d("D :- Main Activity","-onPause");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d("D :- Main Activity","-onResume");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d("D :- Main Activity","-onStop");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d("D :- Main Activity","-onDestroy");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+       super.onSaveInstanceState(outState);
+       state =  getListView().onSaveInstanceState();
+       Log.d("D :- Main Activity","-onSaveInstaceState "+state);
+       outState.putParcelable("listViewState",state);
+       outState.putSerializable("ARRAYLIST",(Serializable) arrayList);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle bundleState){
+        state = bundleState.getParcelable("listViewState");
+        ArrayList<Object> arrayList = (ArrayList<Object>)bundleState.getSerializable("ARRAYLIST");
+        adapter = new ArrayAdapter<Object>(this,R.layout.listitemlayout,arrayList);
+        setListAdapter(adapter);
+         Log.d("D :- Main Activity","-onRestoreInstanceState  "+state);
+        Log.d("D :- Main Activity","-onRestoreInstanceState  listview is"+getListView());
+        getListView().onRestoreInstanceState(state);
+        Log.d("D :- Main Activity","-onRestoreInstanceState  listview is"+getListView());
+    }
+
     public void processFinish(ArrayList<Object> list) {
 
-        Log.d("MainActivity","list is "+list);
-        Log.d("MainActivity","process finish list is "+list);
-        ListAdapter adapter = new ArrayAdapter<Object>(this,R.layout.listitemlayout,list);
+        Log.d("D :- Main Activity","processFinish");
+        //Log.d("MainActivity","list is "+list);
+        //Log.d("MainActivity","process finish list is "+list);
+        arrayList =list;
+        adapter = new ArrayAdapter<Object>(this,R.layout.listitemlayout,list);
         setListAdapter(adapter);
         final ListView listView;
         listView = getListView();
